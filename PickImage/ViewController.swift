6ba9bdf9b22461ffa47a27c711e3f4abb70f0702
duckman1974 <krduckman@bellsouth.net>
2016-11-06 17:48:40 -0500

    Initial Commit

---
+++ PickImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickImage
+//
+//  Created by Candice Reese on 11/6/16.
+//  Copyright © 2016 Kevin Reese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
